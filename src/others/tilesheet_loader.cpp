a32a6c09b999c44f189dc793900dd30d3069b99a
VisualGMQ <2142587070@qq.com>
2022-10-19 00:28:16 +0800

    rewrite all code, because the modify of tinyengine

---
+++ src/others/tilesheet_loader.cpp
@@ -0,0 +1,119 @@
+#include "tilesheet_loader.hpp"
+
+std::string GetFilenameNoExt(const std::string& filename) {
+    auto idx = filename.find_last_of(".");
+    if (idx == filename.npos) return filename;
+    return std::string(filename.data(), idx);
+}
+
+void LoadImageResources(const std::filesystem::path& root, const std::filesystem::path& path) {
+    if (!std::filesystem::exists(path)) return;
+    if (!std::filesystem::is_directory(path)) {
+        Loge("path \"{}\" isn't directory", path);
+        return;
+    }
+    std::filesystem::directory_iterator dirIter(path);
+    while (dirIter != std::filesystem::directory_iterator()) {
+        if (dirIter->is_directory()) {
+            LoadImageResources(root, dirIter->path());
+        } else if (dirIter->is_regular_file() && IsImageFile(dirIter->path())) {
+            std::filesystem::path configPath;
+            if (ExistsTileesheetConfigFile(dirIter->path(), configPath)) {
+                LoadTilesheet(root, dirIter->path(), configPath);
+            } else {
+                LoadImage(root, dirIter->path());
+            }
+        }
+        dirIter++;
+    }
+}
+
+bool ExistsTileesheetConfigFile(const std::filesystem::path& tilesheet, OUT std::filesystem::path& outConfigFilename) {
+    if (tilesheet.extension() == ".toml") return false;
+    outConfigFilename = GetFilenameNoExt(tilesheet.string()) + ".toml";
+    return std::filesystem::exists(outConfigFilename);
+}
+
+std::string RemoveRootFromPath(const std::string& root, const std::string& path) {
+    int i = 0;
+    for (; i < root.size(); i++) {
+        char c1 = root[i];
+        if (c1 == '\\') c1 = '/';
+        char c2 = path[i];
+        if (c2 == '\\') c2 = '/';
+        if (c1 != c2) {
+            break;
+        }
+    }
+    return path.substr(i + 1);
+}
+
+void LoadImage(const std::filesystem::path& root, const std::filesystem::path& filepath) {
+    std::string name = RemoveRootFromPath(root.string(), GetFilenameNoExt(filepath.string()));
+    CvtWindowsDelim2Unix(name);
+    auto texture = engine::TextureFactory::Create(filepath.string());
+    engine::ImageFactory::Create(texture, name, std::nullopt);
+}
+
+void LoadTilesheet(const std::filesystem::path& root,
+                   const std::filesystem::path& tilesheetPath,
+                   const std::filesystem::path& configPath) {
+    engine::Texture* texture = engine::TextureFactory::Create(tilesheetPath.string());
+    if (!texture) {
+        Loge("tilesheet {} load failed", tilesheetPath);
+        return ;
+    }
+
+    auto tbl = toml::parse_file(configPath.string());
+    int row = tbl["row"].value_or(0);
+    int col = tbl["col"].value_or(0);
+    if (row == 0 || col == 0) {
+        Loge("tilesheet \"{}\" config invalid: row == 0 or col == 0", configPath);
+        return ;
+    }
+    if (!tbl["content"].is_table()) {
+        Loge("tilesheet \"{}\" no content");
+        return ;
+    }
+    auto table = tbl["content"].as_table();
+    if (!table) {
+        Loge("tilesheet \"{}\" no content");
+        return ;
+    }
+
+    engine::Size tileSize(texture->Width() / col, texture->Height() / row);
+    for (auto& elem : *table) {
+        auto arr = elem.second.as_array();
+        if (!arr) {
+            Loge("tilesheet \"{}\" config: content's element not contain array", configPath);
+        } else {
+            std::string filepath = GetFilenameNoExt(tilesheetPath.string());
+            CvtWindowsDelim2Unix(filepath);
+            std::string name = RemoveRootFromPath(root.string(), filepath + "#" + std::string(elem.first.str()));
+
+            int x = arr->at(0).as_integer()->value_or(-1);
+            int y = arr->at(1).as_integer()->value_or(-1);
+            if (x == -1 || y == -1) {
+                Loge("tilesheet \"{}\" config: content's x or y is -1", configPath);
+            }
+            engine::ImageFactory::Create(texture, name, engine::Rect(tileSize.w * x, tileSize.h * y, tileSize.w, tileSize.h));
+        }
+    }
+}
+
+bool IsImageFile(const std::filesystem::path& filepath) {
+    if (!filepath.has_extension()) return false;
+    auto extension = filepath.extension();
+    return extension == ".png" ||
+           extension == ".jpg" ||
+           extension == ".jpeg" ||
+           extension == ".bmp" ||
+           extension == ".xmp";
+}
+
+void CvtWindowsDelim2Unix(std::string& text) {
+    for (auto& c : text) {
+        if (c == '\\')
+            c = '/';
+    }
+}
--- src/scenes/start_scene.cpp
+++ src/scenes/start_scene.cpp
@@ -2,9 +2,7 @@
 
 void StartScene::OnInit() {
     loadResources();
-    engine::debug::PropertyWatcher::Attach2Scene();
-    initNodes();
-    engine::debug::DebugAttachSceneTree();
+    LoadImageResources("./resources", "./resources");
 }
 
 void StartScene::OnQuit() {
@@ -12,35 +10,4 @@ void StartScene::OnQuit() {
 }
 
 void StartScene::loadResources() {
-    auto tilesheet = engine::TextureFactory::Create("resources/img/tilesheet.png", "tilesheet");
-    auto role = engine::TextureFactory::Create("resources/img/role.png", "role");
-    engine::TileSheetFactory::Create("tilesheet", tilesheet, 12, 12);
-    engine::TileSheetFactory::Create("role", role, 9, 2);
-}
-
-void StartScene::initNodes() {
-    auto world = engine::World::Instance();
-    auto role = world->CreateEntity("role");
-    role->SetBehavior(std::make_unique<RoleBehavior>());
-    auto node = world->CreateComponent<engine::Node2DComponent>();
-    role->SetComponent(node);
-    Attach2D(role);
-
-    engine::debug::PropertyWatcher::SetWatchEntity(role);
-
-    auto roleHead = world->CreateEntity<engine::SpriteComponent, engine::Node2DComponent>("role head");
-    node->Attach(roleHead);
-
-    auto roleBody = world->CreateEntity<engine::SpriteComponent, engine::Node2DComponent>("role body");
-    node->Attach(roleBody);
-
-    auto roleHair = world->CreateEntity<engine::SpriteComponent, engine::Node2DComponent>("role hair");
-    node->Attach(roleHair);
-
-    auto roleFeet = world->CreateEntity<engine::SpriteComponent, engine::Node2DComponent>("role feet");
-    node->Attach(roleFeet);
-
-    auto sword = world->CreateEntity<engine::Node2DComponent, engine::SpriteComponent>("sword");
-    sword->SetBehavior(std::make_unique<SwordBehavior>());
-    node->Attach(sword);
 }
