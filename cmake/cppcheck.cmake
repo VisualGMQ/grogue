490b36ba68cbee92ee9bfda8ae925557f54e78ee
VisualGMQ <2142587070@qq.com>
2023-01-12 15:38:01 +0800

    fix(build-system):fixed cppcheck found error

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,11 +1,14 @@
-find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-if (CMAKE_CXX_CPPCHECK)
-    list(
-        APPEND CMAKE_CXX_CPPCHECK 
-            "--enable=all"
-            "--inconclusive"
-            "-I ./include"
-            "--force" 
-            "--std=c++17"
-    )
-endif()
+function(AttachCppCheck)
+    find_program(CPPCHECK NAMES cppcheck)
+    if (CPPCHECK)
+        set(CMAKE_CXX_CPPCHECK ${CPPCHECK})
+        list(
+            APPEND CMAKE_CXX_CPPCHECK 
+                "--enable=all"
+                "--inconclusive"
+                "-I ./include"
+                "--force" 
+                "--std=c++17"
+        )
+    endif()
+endfunction()
